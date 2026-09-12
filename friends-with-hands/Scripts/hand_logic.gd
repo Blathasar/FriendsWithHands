98d7c9a073402e5623715769666300f7da4b6675
Blathasar <biblioelias@googlemail.com>
2026-09-12 16:37:49 +0200

    hands logic

--- friends-with-hands/Scripts/hand_logic.gd
+++ friends-with-hands/Scripts/hand_logic.gd
@@ -2,7 +2,16 @@ enum HandSign{
 	ROCK, PAPER, SCIZZOR, THUMBS_UP, NONE
 }
 
-static func convert_finger_positions_to_handsign(finger_positions:Array[bool]) -> HandSign: 
+static func convert_finger_positions_to_handsign(finger_positions:int) -> HandSign: 
+	match finger_positions:
+		0:
+			return HandSign.ROCK
+		11111:
+			return HandSign.PAPER
+		110:
+			return HandSign.SCIZZOR
+		1: 
+			return HandSign.THUMBS_UP
 	return HandSign.NONE
 
 static func beatsHand(attackingHandSign: HandSign, defendingHandSign: HandSign) -> bool:
